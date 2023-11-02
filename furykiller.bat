d2c7d23f54aeeb786fbb43ba590490eb74c10795
musaalif6969 <139727811+musaalif6969@users.noreply.github.com>
2023-11-02 18:45:13 +0600

    Update furykiller.bat

--- furykiller.bat
+++ furykiller.bat
@@ -1,15 +1,19 @@
 @echo off
-:: Check for administrative privileges
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" && (
-    goto :runScript
-) || (
-    echo You need to run this script with administrative privileges.
-    echo Please right-click on the script and select "Run as administrator."
-    pause
-    exit /b
+:: BatchGotAdmin
+:: Detects whether the script is running with administrator privileges
+:: If it's not, relaunch the script with administrator permissions
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
 )
 
-:runScript
+:: Your script code goes here
 taskkill /F /IM Fury_BP.vmp.exe
 cls
 exit
