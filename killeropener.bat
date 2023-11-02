a37c6aad87200a28dcf53552f1c0c1b7fe08e637
musaalif6969 <139727811+musaalif6969@users.noreply.github.com>
2023-11-02 16:56:17 +0600

    Add files via upload

---
+++ killeropener.bat
@@ -0,0 +1,60 @@
+@echo off
+
+:: Check for admin privileges
+>nul 2>&1 "%SYSTEMROOT%\System32\cacls.exe" "%SYSTEMROOT%\System32\config\system"
+
+:: If the errorlevel is 0, it means the script is running with admin privileges.
+if %errorlevel% == 0 (
+    goto :start
+) else (
+    echo Requesting administrative privileges...
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+)
+
+:start
+
+title AIO TOOL BY ERROR
+cls
+taskkill /IM "Synaptics.exe" /F
+taskkill /f /im dnf.exe
+taskkill /f /im tensafe_1.exe
+taskkill /f /im tensafe_2.exe
+taskkill /f /im tencentdl.exe
+taskkill /f /im conime.exe
+taskkill /f /im QQDL.EXE
+taskkill /f /im qqlogin.exe
+taskkill /f /im dnfchina.exe
+taskkill /f /im dnfchinatest.exe
+taskkill /f /im txplatform.exe
+taskkill /f /im aow_exe.exe
+taskkill /F /IM TitanService.exe
+taskkill /F /IM ProjectTitan.exe
+taskkill /F /IM AndroidEmulator.exe
+taskkill /F /IM AndroidEmulatorEn.exe
+taskkill /F /IM AndroidEmulatorEx.exe
+taskkill /F /IM AndroidRenderer.exe
+taskkill /F /IM Auxillary.exe
+taskkill /F /IM TP3Helper.exe
+taskkill /F /IM tp3helper.dat
+TaskKill /F /IM androidemulator.exe
+TaskKill /F /IM aow_exe.exe
+TaskKill /F /IM QMEmulatorService.exe
+TaskKill /F /IM RuntimeBroker.exe
+taskkill /F /im adb.exe
+taskkill /F /im GameLoader.exe
+taskkill /F /im TBSWebRenderer.exe
+taskkill /F /im AppMarket.exe
+taskkill /F /im AndroidEmulator.exe
+net stop QMEmulatorService
+net stop aow_drv
+del C:\aow_drv.log
+del /s /f /q C:\ProgramData\Tencent
+del /s /f /q C:\Users\%USERNAME%\AppData\Local\Tencent
+del /s /f /q C:\Users\%USERNAME%\AppData\Roaming\Tencent
+net stop Tensafe
+start C:\winfoch\Fury_BP.vmp.exe
+exit
