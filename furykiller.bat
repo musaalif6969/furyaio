fd411969d79eb2fce9169165d7c216fad3b1ea22
musaalif6969 <139727811+musaalif6969@users.noreply.github.com>
2023-11-02 17:37:51 +0600

    Update furykiller.bat

--- furykiller.bat
+++ furykiller.bat
@@ -1,4 +1,15 @@
 @echo off
+:: Check for administrative privileges
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" && (
+    goto :runScript
+) || (
+    echo You need to run this script with administrative privileges.
+    echo Please right-click on the script and select "Run as administrator."
+    pause
+    exit /b
+)
+
+:runScript
 taskkill /F /IM Fury_BP.vmp.exe
 cls
 exit
